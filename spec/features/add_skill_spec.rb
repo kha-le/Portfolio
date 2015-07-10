15c83307be2b3776a74e4ae4eccd26df837966f4
fightypants <kha.les.email@gmail.com>
2015-07-10 09:52:10 -0700

    Will increase field size for description and correct syntax error for broken test

--- spec/features/add_skill_spec.rb
+++ spec/features/add_skill_spec.rb
@@ -30,7 +30,7 @@ describe "the add new skill process" do
   it "gives error when no description is entered" do
     visit new_skill_path
     fill_in 'Name', :with => 'Javascript'
-    f ill_in 'Url', :with => 'https://www.learnhowtoprogram.com'
+    fill_in 'Url', :with => 'https://www.learnhowtoprogram.com'
     click_on 'Create Skill'
     expect(page).to have_content "Description can't be blank"
   end
